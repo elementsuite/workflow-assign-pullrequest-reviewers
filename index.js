3e59cb07b03be1765071df2fecae5e894f9363d4
Samuel O'Donnell <samuel.odonnell@elementsuite.com>
2020-01-07 18:55:03 +0000

    console log an object

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 try {
   const token = core.getInput('github-token');
   const groups = core.getInput('groups');
-  console.log('groups', groups);
+  console.log('groups', JSON.parse(groups));
 } catch (error) {
   console.error(error.message);
 }
