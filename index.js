8f0c636b5d88c1459defbd54ef9c8d39b68dec0d
Samuel O'Donnell <samuel.odonnell@elementsuite.com>
2020-01-08 00:53:28 +0000

    adding bulk of code to async function

--- index.js
+++ index.js
@@ -17,38 +17,42 @@ var requestReview = function(client, pullRequest, reviewers, fallbacks, author) 
   });
 }
 
-try {
-  const token = core.getInput('github-token');
-  const titleRegex = core.getInput('title-regex');
-  const groups = JSON.parse(core.getInput('groups'));
-  const titleSplitCharacter = core.getInput('title-split-chara');
-  const client = new github.GitHub(token);
-  const payload = github.context.payload;
-  const pullRequest = github.context.issue;
-  const title = payload.pull_request.title;
-  const author = payload.pull_request.user.login;
+async function run() {
+  try {
+    const token = core.getInput('github-token');
+    const titleRegex = core.getInput('title-regex');
+    const groups = JSON.parse(core.getInput('groups'));
+    const titleSplitCharacter = core.getInput('title-split-chara');
+    const client = new github.GitHub(token);
+    const payload = github.context.payload;
+    const pullRequest = github.context.issue;
+    const title = payload.pull_request.title;
+    const author = payload.pull_request.user.login;
 
-  let commits = await client.pulls.listCommits({
-    owner: pullRequest.owner,
-    repo: pullRequest.repo,
-    pull_number: pullRequest.number
-  });
+    let commits = await client.pulls.listCommits({
+      owner: pullRequest.owner,
+      repo: pullRequest.repo,
+      pull_number: pullRequest.number
+    });
 
-  console.log("adding await", commits);
+    console.log("adding await", commits);
 
-  if (!new RegExp(titleRegex).test(title)) {
-    return;
+    if (!new RegExp(titleRegex).test(title)) {
+      return;
+    }
+
+    var titleElements = title.split(titleSplitCharacter);
+    var module = titleElements[0];
+    var type = titleElements[1];
+
+    var reviewers = groups[module][type];
+
+    if (reviewers && reviewers.length) {
+      requestReview(client, pullRequest, reviewers, groups.default, author)
+    }
+  } catch (error) {
+    console.error(error.message);
   }
-
-  var titleElements = title.split(titleSplitCharacter);
-  var module = titleElements[0];
-  var type = titleElements[1];
-
-  var reviewers = groups[module][type];
-
-  if (reviewers && reviewers.length) {
-    requestReview(client, pullRequest, reviewers, groups.default, author)
-  }
-} catch (error) {
-  console.error(error.message);
 }
+
+run();
